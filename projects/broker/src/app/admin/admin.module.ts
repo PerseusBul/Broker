17c850be12dbfc6951cd0c060cccea6c114fb1aa
PerseusBul <chavdar.peichev@gmail.com>
2022-05-29 11:30:17 +0300

    fixup! Remove death code

--- projects/broker/src/app/admin/admin.module.ts
+++ projects/broker/src/app/admin/admin.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonFormUiModule } from 'projects/shared/common-form-ui.module';
 import { NomSelectModule } from 'projects/shared/components/nom-select/nom-select.module';
 import { ActionServiceModule } from 'projects/shared/services/action-service/action-service.module';
-import { AccountUpdateComponent } from './account-update/account-update.component';
-import { AccountsPreviewComponent } from './accounts-preview/accounts-preview.component';
 import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
-  declarations: [AccountsPreviewComponent, AccountUpdateComponent],
+  declarations: [],
   imports: [CommonModule, AdminRoutingModule, CommonFormUiModule, ActionServiceModule, NomSelectModule]
 })
 export class AdminModule {}
